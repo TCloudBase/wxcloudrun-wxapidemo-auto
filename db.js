3eff3206cbc2320cc7f0a5bbd8711884b7ebf735
fanyinghao <fanyinghao@gmail.com>
2022-01-19 21:46:44 +0800

    fix

--- db.js
+++ db.js
@@ -1,7 +1,7 @@
 const { Sequelize, DataTypes } = require("sequelize");
 
 // 从环境变量中读取数据库配置
-const { MYSQL_USERNAME, MYSQL_PASSWORD, MYSQL_ADDRESS = "" } = process.env;
+const { MYSQL_USERNAME, MYSQL_PASSWORD, MYSQL_ADDRESS } = process.env;
 
 const [host, port] = MYSQL_ADDRESS.split(":");
 
