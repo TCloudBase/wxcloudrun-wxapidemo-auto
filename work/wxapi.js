3b7b6e362fbd07a536dbbc5eefa7878cd7dd185e
mac <wasfzxt@foxmail.com>
2022-03-10 15:23:50 +0800

    fix token

--- work/wxapi.js
+++ work/wxapi.js
@@ -28,60 +28,62 @@ function getToken () {
  * @param {boolean} ssl 是否https调用，默认是，如果是本地调试形态，会自动用否，不建议自主传入
  * @returns {object} 返回内容，api返回直接透传，网络问题则统一包装成与api回包一致
  */
-async function call (name, data, ssl = true) {
+async function call (name, data) {
   console.log(`\n--------- 请求发起【${name}】 ---------`)
   const token = getToken()
-  if (token.token != null) { // 如果token存在，开始发起请求
-    // 包装api请求
-    // ca文件不存在并且ssl为true时，则附加token，否则则不附加token
-    // ssl为false，说明尝试https遇到ssl问题，则证明是本地调试环境，并且开启了开放服务，所以http时无需传入token
-    var options = {
-      method: 'POST',
-      url: `${ssl === true ? 'https' : 'http'}://api.weixin.qq.com/${name}${token.ca == null && ssl === true ? '?cloudbase_access_token=' + token.token : ''}`,
-      headers: {
-        'Content-Type': 'application/json'
-      },
-      body: JSON.stringify(data),
-      ca: token.ca,
-      secureProtocol: 'TLSv1_2_method'
-    }
-    console.log(`${ssl === true ? '发起https请求' : '发起http请求'}，${token.ca == null && ssl === true ? '带token' : '免token'}`)
-    return new Promise((resolve, reject) => {
-      request(options, async function (error, response) {
-        if (error) { // 请求有错误
-          const err = error.toString()
-          if (err.indexOf('SSL') !== -1) { // 筛选错误，如果存在SSL则判定是本地调试环境，且开启开放服务，不支持SSL
-            console.log('本地调试模式，不支持HTTPS，尝试HTTP使用')
-            resolve(await call(name, data, false)) // 转换 http 运行
-          } else { // 其他网络问题
-            console.log('网络请求错误', err)
+  let ssl = true // 默认为https，指定token或云托管服务内均生效
+  if (token.token == null || token.ca != null) { // 本地调试时不存在token，开启接口服务proxy后使用免token模式
+    ssl = false
+  }
+  // 包装api请求
+  var options = {
+    method: 'POST',
+    url: `${ssl === true ? 'https' : 'http'}://api.weixin.qq.com/${name}${ssl === true ? '?cloudbase_access_token=' + token.token : ''}`,
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(data)
+  }
+  console.log(ssl === true ? '发起https请求，带token' : '发起http请求，免token')
+  return new Promise((resolve, reject) => {
+    request(options, async function (error, response) {
+      if (error) { // 请求有错误
+        const err = error.toString()
+        console.log('网络请求错误', err)
+        resolve({ // 返回网络问题
+          errcode: -1,
+          errmsg: err
+        })
+      } else {
+        const seqid = response.headers['x-openapi-seqid']
+        console.log(`${seqid != null ? '开放服务调用｜' + seqid : 'AccessToken调用'}`)
+        console.log('返回结果：', response.body)
+        try {
+          resolve(JSON.parse(response.body))
+        } catch (e) {
+          if (response.body == null || response.body === '') {
+            console.log('本地调试时，需开启接口服务proxy后再试！传统服务器请使用原生微信token！教学戳此：https://www.bilibili.com/video/BV1QS4y1S7C7')
             resolve({ // 返回网络问题
               errcode: -1,
-              errmsg: err
+              errmsg: '请求错误！本地调试时，需开启接口服务proxy后再试！传统服务器请使用原生微信token！'
             })
-          }
-        } else {
-          const seqid = response.headers['x-openapi-seqid']
-          console.log(`${seqid != null ? '开放服务调用｜' + seqid : 'AccessToken调用'}`)
-          console.log('返回结果：', response.body)
-          try {
-            resolve(JSON.parse(response.body))
-          } catch (e) {
-            console.log('网络请求错误', e.toString())
+          } else if (response.body.indexOf('establish') !== -1) {
+            console.log('你的环境服务没有配置出网NAT网关，请前往配置，链接：https://console.cloud.tencent.com/vpc/vpc')
             resolve({ // 返回网络问题
               errcode: -1,
-              errmsg: e.toString()
+              errmsg: '请求错误！你的环境服务没有配置出网NAT网关！'
+            })
+          } else {
+            console.log('网络请求错误', response.body)
+            resolve({ // 返回网络问题
+              errcode: -1,
+              errmsg: response.body
             })
           }
         }
-      })
+      }
     })
-  } else {
-    return {
-      errcode: -110,
-      errmsg: 'access_token is not exist!'
-    }
-  }
+  })
 }
 
 module.exports = {
